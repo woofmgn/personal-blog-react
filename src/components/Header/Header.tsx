50c0531b4bf9f73ced35a96e8b3cfc86166f0900
Denis Vakulenko <woof88@yandex.ru>
2022-11-02 21:12:35 +0500

    feat: added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+
+
+const Header = () => {
+  return (
+    <section className="header">
+      <ul>
+        <li>
+          <a className="header__link" href="/#">ГЛАВНАЯ</a>
+        </li>
+        <li>
+          <a className="header__link" href="/#">СТАТЬИ</a>
+        </li>
+        <li>
+          <a className="header__link" href="/#">ОБО МНЕ</a>
+        </li>
+        <li>
+          <a className="header__link" href="/#">РЕКЛАМА</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Header;

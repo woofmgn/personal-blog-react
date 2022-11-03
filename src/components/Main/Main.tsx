ee4fd3bd34dac830764058bf449be753cc41320e
Denis Vakulenko <woof88@yandex.ru>
2022-11-03 18:23:03 +0500

    feat: added posting section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,7 +5,7 @@ const Main = () => {
       <section className="stories">
         <ul className="stories-container">
           <li className="stories-container__item">
-            <img className="stories-container__image" src={require('../../images/story.png')} alt="#" />
+            <img className="stories-container__image" src={require('../../images/story3.png')} alt="#" />
             <h2 className="stories-container__title">Отдых на природе</h2>
             <p className="stories-container__date">12.03.2022</p>
           </li>
@@ -15,7 +15,7 @@ const Main = () => {
             <p className="stories-container__date">12.03.2022</p>
           </li>
           <li className="stories-container__item">
-            <img className="stories-container__image" src={require('../../images/story.png')} alt="#" />
+            <img className="stories-container__image" src={require('../../images/story3.png')} alt="#" />
             <h2 className="stories-container__title">Переехал в новую квартиру</h2>
             <p className="stories-container__date">12.03.2022</p>
           </li>
@@ -26,8 +26,14 @@ const Main = () => {
           </li>
         </ul>
       </section>
-      <section className="posting">
-
+      <section className="new-post">
+        <form className="posting-form">
+          <input className="posting-form__input" type="text" placeholder="Напишите что-нибудь" />
+          <div className="posting-form__button-container">
+            <button className="posting-form__button posting-form__button_type_upload-photo" type="button"></button>
+            <button className="posting-form__button posting-form__button_type_submit" type='submit'></button>
+          </div>
+        </form>
       </section>
       <section className="articles">
 
